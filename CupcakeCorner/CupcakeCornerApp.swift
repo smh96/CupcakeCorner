4c32d7acad4856f0284419e01b5a0837cf628aea
Sander Haug <sander_haug@hotmail.com>
2023-01-23 16:19:34 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Sander Haug on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
