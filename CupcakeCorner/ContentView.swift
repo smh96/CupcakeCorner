5c5cc890c619bd55fc1ea69888104f20985b5d70
Sander Haug <sander_haug@hotmail.com>
2023-01-23 17:11:27 +0100

    URL Session

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,16 +7,52 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id: \.trackId) {item in
+            VStackLayout(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+                
+            }
+        }.task {
+           await loadData()
         }
-        .padding()
     }
+    
+    func loadData() async {
+        //1: Create URL we want to read
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            fatalError("Something went wrong with the provided link")
+        }
+        
+        //2: fetch data from said URL
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+                
+            //3: Decode data from URL to response data
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            fatalError("Invalid data.")
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
